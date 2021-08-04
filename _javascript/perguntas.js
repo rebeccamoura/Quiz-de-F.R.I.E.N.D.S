ccc7b526ae822f80c90530afd9979781f00b0e5f
Rebecca Moura <rebeccaaruom@gmail.com>
2021-08-04 15:34:35 -0300

    Base do design pronta

---
+++ _javascript/perguntas.js
@@ -0,0 +1,97 @@
+let perguntas = [{
+    pergunta: 'Qual o nome dos quadros de Phoebe?',
+    img: '_imagens/gladysGlynnys.jpg',
+    opcoes: [{
+        resposta: 'Gladys e Lilly',
+        certo: false
+    }, {
+        resposta: 'Gladys e Carly',
+        certo: false
+    }, {
+        resposta: 'Gladys e Glynnis',
+        certo: true
+    }, {
+        resposta: 'Gladys e Glannis',
+        certo: false
+    }]
+}, {
+    pergunta: 'Quem é o último que descobre que Chandler e Monica tem um caso?',
+    img: '_imagens/chandlerEMonica.jpeg',
+    opcoes: [{
+        resposta: 'Joey',
+        certo: false
+    }, {
+        resposta: 'Ross',
+        certo: true
+    }, {
+        resposta: 'Rachel',
+        certo: false
+    }, {
+        resposta: 'Phoebe',
+        certo: false
+    }]
+}, {
+    pergunta: 'Com que idade Chandler tocou em seios pela primeira vez?',
+    img: '_imagens/chandlerEMonica.jpeg',
+    opcoes: [{
+        resposta: '16',
+        certo: false
+    }, {
+        resposta: '17',
+        certo: false
+    }, {
+        resposta: '18',
+        certo: false
+    }, {
+        resposta: '19',
+        certo: true
+    }]  
+}, {
+    pergunta: 'Com que idade Chandler tocou em seios pela primeira vez?',
+    img: '_imagens/chandlerSeios.jpg',
+    opcoes: [{
+        resposta: '16',
+        certo: false
+    }, {
+        resposta: '17',
+        certo: false
+    }, {
+        resposta: '18',
+        certo: false
+    }, {
+        resposta: '19',
+        certo: true
+    }]
+}, {
+    pergunta: 'Com que idade Chandler tocou em seios pela primeira vez?',
+    img: '_imagens/ursulaBuffay.png',
+    opcoes: [{
+        resposta: 'Ursula Buffay',
+        certo: true
+    }, {
+        resposta: 'Phyllis Buffay',
+        certo: false
+    }, {
+        resposta: 'Hortense Buffay',
+        certo: false
+    }, {
+        resposta: 'Blanche Buffay',
+        certo: false
+    }]
+},{
+    pergunta: 'Onde foi o primeiro beijo de Ross e Rachel?',
+    img: '_imagens/rachelRoss.jpg',
+    opcoes: [{
+        resposta: 'Casa de Joey e Chandler',
+        certo: false
+    }, {
+        resposta: 'Casa de Mônica e Rachel',
+        certo: false
+    }, {
+        resposta: 'Museu onde Ross trabalhava',
+        certo: false
+    }, {
+        resposta: 'Central Perk',
+        certo: true
+    }]
+}]
